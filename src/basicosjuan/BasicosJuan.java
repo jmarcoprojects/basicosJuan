d1e88e1b142445f0a5bd341c8270ea67f98a5235
Juan Antonio Marco <jmarcoprojects@gmail.com>
2019-12-06 15:51:43 +0100

    metodo cadenas

--- src/basicosjuan/BasicosJuan.java
+++ src/basicosjuan/BasicosJuan.java
@@ -6,7 +6,30 @@ package basicosjuan;
  * @author J M
  */
 public class BasicosJuan {
+// Método para mostrar ejemplos de tipo cadenas.
     
+    public static void cadenas(){
+    // Tipo String
+        String nombre;
+        String apellidos;
+        
+    // Asignación de valores a las variables String
+
+        nombre = "Juan Antonio";
+        apellidos = "Marco Poveda";
+        
+    // Mostrar en pantalla el cotenido de las variables.
+
+        System.out.println("Mi nombre es " + nombre );
+        System.out.println("Y mis apellidos son " + apellidos );
+        
+        
+     }
+    
+    
+    public static void main(String[] args) {
+        cadenas();
+    }   
     
 
     
