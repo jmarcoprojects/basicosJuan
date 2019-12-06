b99142dedb9199ed37ca2dcd868eb52315933b58
Juan Antonio Marco <jmarcoprojects@gmail.com>
2019-12-06 15:54:40 +0100

    metodo logicos

--- src/basicosjuan/BasicosJuan.java
+++ src/basicosjuan/BasicosJuan.java
@@ -7,7 +7,29 @@ package basicosjuan;
  */
 public class BasicosJuan {
     
+// Metodo para mostrar ejemplos de tipo logicos
     
+    public static void logicos(){
+
+//Variable tipo boolean       
+        boolean variable;
+            
+//Asignacion de valor
+        variable = true;
+        
+//Mostrar en pantalla el uso del tipo boolean
+
+        if(variable){
+            System.out.println("La variable es verdadera");
+        }
+        else{
+            System.out.println("La variable es falsa");
+        }
+    }
+   
+    public static void main(String[] args) {
+        logicos();
+    }    
 
     
    
