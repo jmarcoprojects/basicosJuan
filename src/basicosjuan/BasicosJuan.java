3b19a7ad366200c799a8f44b990106737040da55
Juan Antonio Marco <jmarcoprojects@gmail.com>
2019-12-06 15:48:49 +0100

    metodo numericos

--- src/basicosjuan/BasicosJuan.java
+++ src/basicosjuan/BasicosJuan.java
@@ -6,10 +6,26 @@ package basicosjuan;
  * @author J M
  */
 public class BasicosJuan {
-    
-    
+// Metodo para ejemplos de tipos numericos.
+     
+    public static void numericos() {
 
-    
-   
-    
+    //Tipos basicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+    //Ejemplos de asignacion de valores a las variables de tipos basicos
+        elMasPeque = 45;
+        entero = 10;
+        doble = 5.6;
+
+    //Mostrar en pantalla el contendido de las variables de tipos basicos.
+        System.out.println("Valor tipo byte: " + elMasPeque);
+        System.out.println("Valor entero: " + entero);
+        System.out.println("Valor doble: " + doble);
+    }   
 }
